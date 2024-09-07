3e69a2813633fe8ec401bc73165fd377db3791d7
Anett Kneifel <kneiane@gmail.com>
2024-09-07 21:09:21 +0200

    4.Handling multiple threads solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   combineLatest,
+  concatAll,
   debounceTime,
   filter,
   forkJoin,
   map,
+  merge,
   Observable,
   Subject,
   Subscription,
@@ -34,7 +36,6 @@ export class AppComponent implements OnInit, OnDestroy {
   changeCharactersInput(element: any): void {
     // 1.1. Add functionality to changeCharactersInput method. Changes searchTermByCharacters Subject value on input change.
     const inputValue: string = element.target.value;
-    console.log(inputValue);
     // YOUR CODE STARTS HERE
     this.searchTermByCharacters.next(inputValue);
     // YOUR CODE ENDS HERE
@@ -42,9 +43,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   initCharacterEvents(): void {
     // 1.2. Add API call on each user input. Use mockDataService.getCharacters - to make get request.
-
     // 2. Since we don't want to spam our service add filter by input value and do not call API until a user enters at least 3 chars.
-    
     // 3. Add debounce to prevent API calls until user stop typing.
 
     this.charactersResults$ = this.searchTermByCharacters
@@ -56,7 +55,6 @@ export class AppComponent implements OnInit, OnDestroy {
           map((x) => this.mockDataService.getCharacters(x),
         )
         )
-        
         // YOUR CODE ENDS HERE
         ;
   }
@@ -65,6 +63,14 @@ export class AppComponent implements OnInit, OnDestroy {
     // 4. On clicking the button 'Load Characters And Planets', it is necessary to process two requests and combine the results of both requests into one result array. As a result, a list with the names of the characters and the names of the planets is displayed on the screen.
     // Your code should looks like this: this.planetAndCharactersResults$ = /* Your code */
     // YOUR CODE STARTS HERE
+    // this.planetAndCharactersResults$ = this.mockDataService.getCharacters();
+
+    this.planetAndCharactersResults$ = forkJoin([
+      this.mockDataService.getCharacters(),
+      this.mockDataService.getPlanets()
+    ]).pipe(map(x => [].concat(...x)));
+    
+    this.planetAndCharactersResults$.subscribe(console.log)
     // YOUR CODE ENDS HERE
   }
 
