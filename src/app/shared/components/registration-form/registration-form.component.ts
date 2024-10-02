e1ce697549d18c4e394501023712b9095e52383c
Anett Kneifel <kneiane@gmail.com>
2024-10-02 10:04:35 +0200

    code cleanup

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { emailMatchRegex } from '@app/shared/directives/email.directive';
+import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { emailMatchRegex } from "@app/shared/directives/email.directive";
 import { AuthService } from "@app/auth/services/auth.service";
-import { Router } from '@angular/router';
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-registration-form',
-  templateUrl: './registration-form.component.html',
-  styleUrls: ['./registration-form.component.scss'],
+  selector: "app-registration-form",
+  templateUrl: "./registration-form.component.html",
+  styleUrls: ["./registration-form.component.scss"],
 })
 export class RegistrationFormComponent {
   registrationForm!: FormGroup;
@@ -16,39 +16,34 @@ export class RegistrationFormComponent {
 
   ngOnInit(): void {
     this.registrationForm = new FormGroup({
-      name: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-      ]),
-      email: new FormControl('', [
+      name: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      email: new FormControl("", [
         Validators.required,
         // emailMatchRegex()
       ]),
-      password: new FormControl('', [
-        Validators.required
-      ]),
+      password: new FormControl("", [Validators.required]),
     });
-    
   }
 
   onSubmit() {
     if (this.registrationForm.valid) {
-      this.authService.register({
-        name: this.registrationForm.controls["name"].value,
-        email: this.registrationForm.controls["email"].value,
-        password: this.registrationForm.controls["password"].value,
-      }).subscribe(
-        {
-          next: () => { this.router.navigate(["/login"]); },
-        }
-      );
+      this.authService
+        .register({
+          name: this.registrationForm.controls["name"].value,
+          email: this.registrationForm.controls["email"].value,
+          password: this.registrationForm.controls["password"].value,
+        })
+        .subscribe({
+          next: () => {
+            this.router.navigate(["/login"]);
+          },
+        });
     } else {
       console.log("Form is invalid");
     }
   }
 
   submitForm() {
-    this.onSubmit()
+    this.onSubmit();
   }
-
 }
